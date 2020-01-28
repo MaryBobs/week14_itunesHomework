b3f1df02fd5c52deb5833e8ec2381a277e191474
Catrin <catrinhughes92@gmail.com>
2020-01-28 15:09:56 +0000

    calls itunes api

--- src/containers/MusicBox.js
+++ src/containers/MusicBox.js
@@ -3,6 +3,22 @@ import MusicList from '../components/MusicList';
 
 class MusicBox extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            songs: []
+        };
+    }
+
+    componentDidMount() {
+        const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"
+
+        fetch(url)
+            .then(res => res.json())
+            .then(songs => this.setState({ songs: songs }))
+            .catch(err => console.err());
+    }
+
     render() {
         return (
             <div>
