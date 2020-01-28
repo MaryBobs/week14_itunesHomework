615d31ff890281182324216ed077cef4ff3396a6
Catrin <catrinhughes92@gmail.com>
2020-01-28 15:01:23 +0000

    created MusicBox class

---
+++ src/containers/MusicBox.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class MusicBox extends Component {
+
+    render() {
+        return(
+            <p>MusicBox</p>
+        )
+    }
+
+
+}
+
+export default MusicBox;
